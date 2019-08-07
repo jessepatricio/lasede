2d35ca590252e08d4e746e73686dc65f776cbf7b
jessepatricio <jessepatricio@gmail.com>
2019-08-07 19:27:39 +1200

    added sidedrawer and pull readme file from remote

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 
+
 import AppBar from '@material-ui/core/AppBar';
-import ToolBar from '@material-ui/core/Toolbar';
-import MenuIcon from '@material-ui/core/Menu';
+import Toolbar from '@material-ui/core/Toolbar';
+import MenuIcon from '@material-ui/icons/Menu';
 import IconButton from '@material-ui/core/IconButton';
 
+import SideDrawer from './SideDrawer';
+
 class Header extends Component {
+
+    state = {
+        drawerOpen: false
+    }
+
+    toggleDrawer = (value) => {
+        this.setState({
+            drawerOpen: value
+        })
+    }
     render() {
         return (
             <AppBar
@@ -16,13 +29,24 @@ class Header extends Component {
                     padding: '10px 0px'
                 }}
                 >
-                <ToolBar>
+                <Toolbar>
                     <div className="header_logo">
                         <div className="font_righteous header_logo_venue">La Sede</div>
                         <div className="header_logo_title">Concierto Locasion</div>
                     </div>
-
-                </ToolBar>
+                    <IconButton
+                        aria-label="menu"
+                        color="inherit"
+                        onClick={ ()=> this.toggleDrawer(true)}
+                        >
+                    
+                        <MenuIcon />
+                    </IconButton>
+                    <SideDrawer
+                        open={this.state.drawerOpen}
+                        onClose={(value) => this.toggleDrawer(value)}
+                    />
+                </Toolbar>
             </AppBar>
         );
     }
