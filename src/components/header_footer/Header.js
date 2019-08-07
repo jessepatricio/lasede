420ea1119b8a34a1bb169169cc414f25aaa8475f
jessepatricio <jessepatricio@gmail.com>
2019-08-07 19:47:12 +1200

    completed header

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -11,20 +11,34 @@ import SideDrawer from './SideDrawer';
 class Header extends Component {
 
     state = {
-        drawerOpen: false
+        drawerOpen: false,
+        headerShow: false
     }
 
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll);
+    }
+
+    handleScroll = () => {
+        if (window.scrollY > 0) {
+            this.setState({ headerShow: true })
+        }else {
+            this.setState({ headerShow: false })
+        }
+    }
+    
     toggleDrawer = (value) => {
         this.setState({
             drawerOpen: value
         })
     }
+
     render() {
         return (
             <AppBar
                 position="fixed"
                 style={{
-                    backgroundColor:'#2f2f2f',
+                    backgroundColor: this.state.headerShow ? '#2f2f2f' : 'transparent',
                     boxShadow: 'none',
                     padding: '10px 0px'
                 }}
